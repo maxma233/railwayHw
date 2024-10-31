9401ab4ac65caafef48373796fe755545e0f5e05
Max Ma <maxma@u.boisestate.edu>
2024-10-31 13:43:37 -0600

    dbChange

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -82,7 +82,7 @@ public class HerokuApplication {
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
+        output.add("Read from DB: " + rs.getTimestamp("tick") + ", Random String: " + rs.getString("random_string"));
       }
 
       model.put("records", output);
